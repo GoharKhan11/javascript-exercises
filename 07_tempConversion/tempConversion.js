23d5d23b81da30d138fdc6eeff7a70d189f54aca
Gohar <goharayub2008@gmail.com>
2022-07-22 05:12:41 -0400

    Add solved 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,24 @@
-const ftoc = function() {
+function roundToOneDecimal (baseInt) {
+  /* int -> int
+  Rounds baseInt to one decimal place.
+  */
+  return (Math.round(baseInt * 10) / 10);
+}
 
+const ftoc = function(fahrenheitInt) {
+  /* int -> int
+  Converts fahrenheitInt from fahrenheit to celsius
+  */
+  let convertedInt = ((fahrenheitInt - 32) * (5/9));
+  return roundToOneDecimal(convertedInt);
 };
 
-const ctof = function() {
-
+const ctof = function(celsiusInt) {
+  /*
+  Converts celsiusInt from celsius to fahrenheit
+  */
+  let convertedInt = ((celsiusInt * (9/5)) + 32);
+  return roundToOneDecimal(convertedInt);
 };
 
 // Do not edit below this line
