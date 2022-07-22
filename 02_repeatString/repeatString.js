3c0d31db46faeec2b76392200b50e9b51a1ab4e1
Gohar <goharayub2008@gmail.com>
2022-07-22 02:08:43 -0400

    Add solved 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,17 @@
-const repeatString = function() {
-
-};
+const repeatString = function(baseString, repetitions) {
+    if (repetitions >= 0) {
+        // Create a result string
+        let result = "";
+        // repeat loop to add basestring to result desired times
+        for (let i=0; i < repetitions; i++) {
+            result = result + baseString;
+        }
+        return result;
+    }
+    else {
+        return "ERROR";
+    }
+}
 
 // Do not edit below this line
 module.exports = repeatString;
