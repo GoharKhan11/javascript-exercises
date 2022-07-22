b6cbadccf3d090d6fd5b14bb9e1db2af9313e312
Gohar <goharayub2008@gmail.com>
2022-07-22 05:01:54 -0400

    Add solved 06_leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,12 @@
-const leapYears = function() {
+const leapYears = function(testYear) {
+    /* int -> bool
+    Returns true if the testYear is a leap year,
+    else returns false. */
 
+    if ((testYear%4 != 0) || (testYear%100 == 0 && testYear%400 != 0)) {
+        return false;
+    }
+    else {return true;}
 };
 
 // Do not edit below this line
