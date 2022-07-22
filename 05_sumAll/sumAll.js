1149e6dbda0d131adff9dd0ef323e5604aa4e66f
Gohar <goharayub2008@gmail.com>
2022-07-22 04:25:51 -0400

    Add solved 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,35 @@
-const sumAll = function() {
+const sumAll = function(int1, int2) {
+    /* int, int -> int
+    Adds integers between startInt
+    and endInt (inclusive) and returns the sum.
+    */
 
+    if ((int1 < 0) || (int2 < 0) || !(typeof int1 == "number") || !(typeof int2 == "number")) {
+        return "ERROR";
+    }
+
+    // Var to store sum
+    let result = 0;
+    // Int to start summation at
+    let startInt;
+    // Int to end summation at
+    let endInt;
+
+    // Stores smaller integer in startInt and larger int in endInt
+    if (int1 <= int2) {
+        startInt = int1;
+        endInt = int2;
+    }
+    else {
+        startInt = int2;
+        endInt = int1;
+    }
+
+    // Goes through integers from startInt to endInt (inclusive)
+    for ( let i=startInt; i <= endInt; i++) {
+        result += i;
+    }
+    return result;
 };
 
 // Do not edit below this line
