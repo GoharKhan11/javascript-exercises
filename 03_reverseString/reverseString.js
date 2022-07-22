79b32a972a43551b266b1b291d459b82d0ebfaca
Gohar <goharayub2008@gmail.com>
2022-07-22 03:21:54 -0400

    Add solved 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,18 @@
-const reverseString = function() {
-
+const reverseString = function(baseString) {
+    // Break letters of string into array elements
+    let baseArray = baseString.split("");
+    // Create array to hold reversed letters
+    let reverseArray = [];
+    // Store number of character being reverse for loop
+    let numberOfCharacters = baseArray.length
+    // Enter the letters into reverseArray from baseArray
+    for (let i=0; i < numberOfCharacters; i++) {
+        reverseArray.push(baseArray.pop());
+    }
+    // Create string using reverseArray
+    let result = reverseArray.join("");
+    // Return reversed string
+    return result;
 };
 
 // Do not edit below this line
